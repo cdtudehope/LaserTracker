b39552a0c44edc9b914cb9d5dcd938c07bc340e1
ChrisTudehope <christopher.tudehope@jacks.sdstate.edu>
2018-02-11 17:46:52 -0600

    Test commit. Hello World print out.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <iostream>
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    cout<<"Hello World!"<<endl;
     MainWindow w;
     w.show();
 
