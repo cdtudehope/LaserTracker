14f51c4cfdbcbc5caacfd471cb1eebc78e7d87bb
ChrisTudehope <christopher.tudehope@jacks.sdstate.edu>
2018-02-12 15:10:18 -0600

    Test Commit

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    cout<<"Hello Worljjjjd!"<<endl;
+    cout<<"Hello Daud!"<<endl;
     MainWindow w;
     w.show();
 
