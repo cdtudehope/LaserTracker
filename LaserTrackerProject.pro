14f605ec93376758cea4d6a9a9821e867d95325b
ChrisTudehope <christopher.tudehope@jacks.sdstate.edu>
2018-02-15 15:35:14 -0600

    Changed file names

--- LaserTrackerProject.pro
+++ LaserTrackerProject.pro
@@ -1,6 +1,6 @@
 #-------------------------------------------------
 #
-# Project created by QtCreator 2018-02-11T17:19:36
+# Project created by QtCreator 2017-03-05T12:30:06
 #
 #-------------------------------------------------
 
@@ -12,7 +12,7 @@ TARGET = LaserTrackerProject
 TEMPLATE = app
 
 # The following define makes your compiler emit warnings if you use
-# any feature of Qt which has been marked as deprecated (the exact warnings
+# any feature of Qt which as been marked as deprecated (the exact warnings
 # depend on your compiler). Please consult the documentation of the
 # deprecated API in order to know how to port your code away from it.
 DEFINES += QT_DEPRECATED_WARNINGS
@@ -23,12 +23,35 @@ DEFINES += QT_DEPRECATED_WARNINGS
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 
-SOURCES += \
-        main.cpp \
+SOURCES += main.cpp\
         mainwindow.cpp
 
-HEADERS += \
-        mainwindow.h
+HEADERS  += mainwindow.h
 
-FORMS += \
-        mainwindow.ui
+FORMS    += mainwindow.ui
+
+INCLUDEPATH += C:\opencv\build\include
+
+LIBS += C:\opencv-build\bin\libopencv_core331.dll
+LIBS += C:\opencv-build\bin\libopencv_highgui331.dll
+LIBS += C:\opencv-build\bin\libopencv_imgcodecs331.dll
+LIBS += C:\opencv-build\bin\libopencv_imgproc331.dll
+LIBS += C:\opencv-build\bin\libopencv_features2d331.dll
+LIBS += C:\opencv-build\bin\libopencv_calib3d331.dll
+
+# more correct variant, how set includepath and libs for mingw
+# add system variable: OPENCV_SDK_DIR=D:/opencv/build
+# read http://doc.qt.io/qt-5/qmake-variable-reference.html#libs
+
+#INCLUDEPATH += $$(OPENCV_SDK_DIR)/include
+
+#LIBS += -L$$(OPENCV_SDK_DIR)/x86/mingw/lib \
+#        -lopencv_core331        \
+#        -lopencv_highgui331     \
+#        -lopencv_imgcodecs331   \
+#        -lopencv_imgproc331     \
+#        -lopencv_features2d331  \
+#        -lopencv_calib3d331
+
+DISTFILES += \
+    ../../Pictures/Camera Roll/WIN_20180111_15_53_13_Pro.jpg
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,66 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <iostream>
+#include <sys/time.h>
+#include <string>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+using namespace std;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    std::string path = "../LaserTracker/TestImages/";
+    std::string imageName = "0";
+    std::string ext = ".jpg";
+    double elapsed_time, start_time;
+    struct timeval tv;
+    double min, max;
+    Point min_loc, max_loc;
+    double averageTime = 0;
+    for(int i = 0; i < 33; i++)
+    {
+        if(i != 2)
+        {
+            imageName = "T" + std::to_string(i);
+            // read an image
+            gettimeofday(&tv, NULL);
+            start_time = double(tv.tv_sec) + double(tv.tv_usec) * 0.000001;
+
+            Mat image = imread(path + imageName + ext, CV_LOAD_IMAGE_COLOR);
+            Mat bgr[3];
+            Mat cropImage;
+            Mat dst;
+            Rect cropRectangle = Rect(172,122,520,278);
+            Scalar meanRedPixelBrightness;
+            cropImage = image(cropRectangle);
+            imshow("My Image" + imageName, cropImage);
+            gettimeofday(&tv, NULL);
+            start_time = double(tv.tv_sec) + double(tv.tv_usec) * 0.000001;
+            split(cropImage, bgr);
+            meanRedPixelBrightness = mean(bgr[2]);
+            minMaxLoc(bgr[2], &min, &max, &min_loc, &max_loc);
+            cout<<i<<"   "<<meanRedPixelBrightness[0]<<"   "<<max<<"   "<<meanRedPixelBrightness[0]+(max-meanRedPixelBrightness[0])*0.96<<"  ";
+            threshold(bgr[2], dst, meanRedPixelBrightness[0]+(max-meanRedPixelBrightness[0])*0.96, 255, THRESH_TOZERO);
+            imshow("threashold" + imageName, dst);
+
+            gettimeofday(&tv, NULL);
+            elapsed_time = (double(tv.tv_sec) + double(tv.tv_usec) * 0.000001) - start_time;
+            averageTime +=elapsed_time;
+            cout<<elapsed_time<<endl;
+        }
+    }
+    averageTime = averageTime/34;
+    cout<<"Average Time: "<<averageTime<<endl;
+    cout<<"Average Frames per Second: "<<1/averageTime<<endl;
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
